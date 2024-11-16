83037b1e01f691eeee1b75f95ba29cfb7637e6e8
aishwarya s <aishwarya9757@gmail.com>
2024-11-16 20:37:45 +0530

    CropImage_AudioRecording

---
+++ lib/screens/widgets/message_animation_widget.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class MessageAnimationWidget extends StatefulWidget {
+  final Widget child;
+  final bool isUser;
+
+  const MessageAnimationWidget({
+    Key? key,
+    required this.child,
+    required this.isUser,
+  }) : super(key: key);
+
+  @override
+  _MessageAnimationWidgetState createState() => _MessageAnimationWidgetState();
+}
+
+class _MessageAnimationWidgetState extends State<MessageAnimationWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _slideAnimation;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    // Scale animation
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutBack,
+      ),
+    );
+
+    // Slide animation - messages slide in from left or right
+    _slideAnimation = Tween<double>(
+      begin: widget.isUser ? 50.0 : -50.0,
+      end: 0.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutCubic,
+      ),
+    );
+
+    // Opacity animation
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    // Start the animation
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(_slideAnimation.value, 0),
+          child: Transform.scale(
+            scale: _scaleAnimation.value,
+            alignment: widget.isUser ? Alignment.centerRight : Alignment.centerLeft,
+            child: Opacity(
+              opacity: _opacityAnimation.value,
+              child: child,
+            ),
+          ),
+        );
+      },
+      child: widget.child,
+    );
+  }
+}
